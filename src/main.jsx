4313b0ab12976e56963cf6b65a72a0b62db97806
priya171299 <priyakanwar2001@gmail.com>
2025-09-01 11:08:26 +0530

    worked in main .jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import "./index.css";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "./context/AppContext.jsx";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix for marker icons in production build
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
