f6d622e81e50686c64cde8f1d812c03e85685974
Deepak <deepak.sharma@parasmesoft.in>
2025-11-03 12:36:56 +0530

    s

--- src/Pages/Reviews/Admin/AdminReview.jsx
+++ src/Pages/Reviews/Admin/AdminReview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AdminReview = () => {
     return (
         <div className='main main_page p-4 md:p-6 min-h-screen duration-900'>
-            now work pending on that page 
+            now work pending on that page redirect on reviews pages 
         </div>
     );
 }
